100930da38e6fa26f5a2ec51a179e5d16c69f18b
NafisFuadTanvir <nafisf026@gmail.com>
2022-12-08 21:13:12 +0600

    second push

---
+++ ipv432bitBinaryTodottedDecimal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int ipv4ToDecimal(long long n){
+    int decimal = 0, i = 0, rem;
+    
+    while(n!=0){
+        rem=n%10;
+        n=n/10;
+        decimal=decimal+rem*pow(2,i);
+        i++;
+        }
+    
+    return decimal;
+    
+    
+}
+
+
+
+int main() {
+   long long a,b,c,d;
+   char dot='.';
+   cout<<"enter the ipv4 binary numbers:--";
+   cin>>a>>b>>c>>d;
+   
+ int decimalOfa=ipv4ToDecimal(a);
+ int decimalOfb=ipv4ToDecimal(b);  
+ int decimalOfc=ipv4ToDecimal(c);
+ int decimalOfd=ipv4ToDecimal(d);
+      
+   cout<<"the dotted decimal numbers of the given ipv4 binary number is:--"<<endl<<decimalOfa<<""<<dot<<decimalOfb<<""<<dot<<decimalOfc<<""<<dot<<decimalOfd;
+
+    return 0;
+}
